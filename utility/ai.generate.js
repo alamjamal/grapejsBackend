3aa9015d21aa8ba91d8a7f7c446841b49280f771
Tahseen Zaid <tahseennz@weblineapps.com>
2023-04-08 14:49:23 +0530

    Fixed API key

--- utility/ai.generate.js
+++ utility/ai.generate.js
@@ -1,5 +1,5 @@
 const { Configuration, OpenAIApi } = require('openai');
-const VITE_Open_AI_Key = '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';
+const VITE_Open_AI_Key = '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';
 const configuration = new Configuration({
   apiKey: VITE_Open_AI_Key,
 });
@@ -7,7 +7,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export const generateImage = async (prompt) => {
-  return await openai.createImage({
+  const response =  await openai.createImage({
     prompt,
     n: 1,
     size: '256x256',
